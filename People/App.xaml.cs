704a6316f5284856bcfac472cc7373f93e771527
Jason Dunn <jasondunn814@gmail.com>
2023-10-20 15:21:57 -0400

    .netmaui workshop Store Local Data Sqlite

---
+++ People/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace People;
+
+public partial class App : Application
+{
+    // TODO: Add a public static PersonRepository property
+    public static PersonRepository PersonRepo { get; private set; }
+
+    public App(PersonRepository repo)
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+
+        // TODO: Initialize the PersonRepository property with the PersonRespository singleton object
+        PersonRepo = repo;
+    }
+}
---
+++ People/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿namespace People;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		// TODO: Add statements for adding PersonRepository as a singleton
+		string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+		builder.Services.AddSingleton(s => ActivatorUtilities.CreateInstance<PersonRepository>(s, dbPath));
+			
+        return builder.Build();
+	}
+}
